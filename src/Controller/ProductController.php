2062825a9190183513a8152711c5f525f43b6fef
Luis Navarro <luisnavarro-03@hotmail.com>
2018-08-01 13:53:30 -0500

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,7 +9,14 @@ use App\Entity\Product;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Doctrine\DBAL\Driver\Connection;
-
+use App\Entity\Task;
+use App\Entity\categorias;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProductController extends Controller
 {
@@ -25,9 +32,62 @@ class ProductController extends Controller
 		
     }
 	
-	 public function new_product()
+	 public function new_product(Connection $connection, Request $request)
     {	
-	 return $this->render('Product/new.html.twig');
+		
+		  // creates a task and gives it some dummy data for this example
+        $task = new Task();
+		 
+		 $categorias = $connection->fetchAll('select * from categorias');
+
+        $form = $this->createFormBuilder($task)
+            ->add('codigo', TextType::class, array(
+    								'attr' => array('class' => 'col s12', 'value'=>'')))			
+			->add('categoria', EntityType::class, array(
+				'class' => Categorias::class,
+				'choice_label' => 'nombre',
+			))			
+           ->add('nombre', TextType::class, array(
+    								'attr' => array('class' => 'col s12', 'value'=>'')))
+			->add('descripcion', TextType::class, array(
+    								'attr' => array('class' => 'col s12', 'value'=>'')))			
+			->add('marca', TextType::class, array(
+    								'attr' => array('class' => 'col s12', 'value'=>'')))
+			->add('precio', TextType::class, array(
+    								'attr' => array('class' => 'col s12', 'value'=>'')))
+            ->add('Guardar', SubmitType::class,  array(
+    								'attr' => array('class' => 'btn blue center-align col s12 m6 offset-m3', 'value'=>'')))
+            ->getForm();
+		 
+
+		 	$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid()) {			
+				$task = $form->getData();
+				
+				$contador=$connection->fetchAll('select * from product where codigo="'.$task->getCodigo().'" or nombre = "'.$task->getNombre().'"');
+				if(count($contador)>0){
+					return $this->render('Product/new.html.twig', array(
+						'form' => $form->createView(), 'categorias' => $categorias, 'error' => 'Ya existe un producto con este codigo o nombre'
+					));
+				}else{
+					$query="INSERT INTO `product` (`id`, `categoria_id_id`, `codigo`, `nombre`, `descripcion`, `marca`, `precio`) VALUES (NULL, '".$task->getCategoria()->getId()."', '".$task->getCodigo()."', '".$task->getNombre()."', '".$task->getDescripcion()."', '".$task->getMarca()."', '".$task->getPrecio()."');";
+					
+					
+					$insert=$connection->fetchAll($query);
+					dd($insert); 
+					return $this->redirectToRoute('Index');
+				}
+				
+			}
+
+		 
+        return $this->render('Product/new.html.twig', array(
+            'form' => $form->createView(), 'categorias' => $categorias
+        ));
+		 
+		 
+	 
 	 }
 	
 
